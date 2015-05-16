d4b6ab01c4b85033461330f87431d0a2bcb56b0b
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-05-16 22:06:05 +0800

    add dashboard page

---
+++ app/templates/client/source/app/dashboard/dashboard.controller.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.dashboard')
+        .controller('DashboardController', DashboardController);
+
+    DashboardController.$inject = [];
+    /* @ngInject */
+    function DashboardController() {
+        var vm = this;
+
+    }
+})();
---
+++ app/templates/client/source/app/dashboard/dashboard.route.js
@@ -0,0 +1,38 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.dashboard')
+        .run(appRun);
+
+    /* @ngInject */
+    function appRun(routerHelper) {
+        routerHelper.configureStates(getStates());
+    }
+
+    function getStates() {
+        return [
+            {
+                state: 'root.dashboard',
+                config: {
+                    url: '/dashboard',
+                    views: {
+                        'main@': {
+                            templateUrl: 'static/dashboard/dashboard.html',
+                            controller: 'DashboardController as vm'
+                        }
+                    },
+                    data: {
+                        title: 'Dashboard',
+                        _class: 'dashboard'
+                    },
+                    sidebar: {
+                        icon: 'mdi-view-dashboard',
+                        text: 'Dashboard'
+                    },
+                    breadcrumb: 'Dashboard'
+                }
+            }
+        ];
+    }
+})();
