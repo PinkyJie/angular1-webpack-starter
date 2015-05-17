b98b67e077141408b9629f70f18e3f18b86a434e
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-05-17 17:48:33 +0800

    handle login/logout

--- app/templates/client/source/app/login/login.controller.js
+++ app/templates/client/source/app/login/login.controller.js
@@ -5,9 +5,57 @@
         .module('app.login')
         .controller('LoginController', LoginController);
 
-    LoginController.$inject = ['routerHelper'];
+    LoginController.$inject = ['User', '$state', 'ajaxErrorHanlder'];
     /* @ngInject */
-    function LoginController(routerHelper) {
+    function LoginController(User, $state, ajaxErrorHanlder) {
         var vm = this;
+
+        vm.login = login;
+
+        var _routeAfterLogin = 'root.dashboard';
+
+        init();
+
+        ////////////
+
+        function init () {
+            // handle logout
+            var action = $state.params.action;
+            if (action === 'logout') {
+                User.logout()
+                    .then(function () {
+                        _setError('success', 'You have been successfully logged out!');
+                    });
+            } else {
+                // check login status firstly
+                User.checkLoggedInStatus()
+                    .then(function () {
+                        $state.go(_routeAfterLogin);
+                    });
+            }
+        }
+
+        function login (credential) {
+            User.login(credential.email, credential.password)
+                .then(success)
+                .catch(error);
+
+            function success (data) {
+                vm.loginError = null;
+                $state.go(_routeAfterLogin);
+            }
+
+            function error (reason) {
+                var message = ajaxErrorHanlder.getMessage(reason);
+                _setError('error', message);
+            }
+        }
+
+        function _setError (type, text) {
+            vm.loginError = {
+                type: type,
+                text: text
+            };
+        }
     }
 })();
