bb09da7feebe5a1dfed04150e91a1653f7919fe5
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-05-17 17:47:48 +0800

    update header based on auth status

--- app/templates/client/source/app/layout/header.controller.js
+++ app/templates/client/source/app/layout/header.controller.js
@@ -5,15 +5,34 @@
         .module('app.layout')
         .controller('HeaderController', HeaderController);
 
-    HeaderController.$inject = ['$state', '$rootScope', 'User'];
+    HeaderController.$inject = ['$state', '$rootScope', 'User', 'Event'];
     /* @ngInject */
-    function HeaderController($state, $rootScope, User) {
+    function HeaderController($state, $rootScope, User, Event) {
         var vm = this;
 
         vm.go = $state.go;
         vm.switchSidebar = switchSidebar;
 
+        init();
+
         ////////////
+
+        function init () {
+            // udpate header based on auth event
+            $rootScope.$on(Event.AUTH_LOGIN, _updateHeader);
+            $rootScope.$on(Event.AUTH_LOGOUT, _updateHeader);
+            $rootScope.$on(Event.AUTH_SESSION_VALID, _updateHeader);
+        }
+
+        function _updateHeader (e, userInfo) {
+            if (userInfo) {
+                vm.isLoggedIn = true;
+                vm.userInfo = userInfo;
+            } else {
+                vm.isLoggedIn = false;
+                vm.userInfo = null;
+            }
+        }
 
         function switchSidebar () {
             $rootScope.showSidebar = !$rootScope.showSidebar;
