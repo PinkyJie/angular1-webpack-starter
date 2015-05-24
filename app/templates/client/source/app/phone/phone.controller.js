852d62c141219aea4f974ae0f0e80e6082c315a7
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-05-24 21:08:25 +0800

    add delete phone feature

--- app/templates/client/source/app/phone/phone.controller.js
+++ app/templates/client/source/app/phone/phone.controller.js
@@ -5,20 +5,55 @@
         .module('app.phone')
         .controller('PhoneController', PhoneController);
 
-    PhoneController.$inject = ['phoneAPI'];
+    PhoneController.$inject = ['phoneAPI', 'ajaxErrorHanlder', 'LxNotificationService'];
     /* @ngInject */
-    function PhoneController (phoneAPI) {
+    function PhoneController (phoneAPI, ajaxErrorHanlder, LxNotificationService) {
         var vm = this;
+
+        vm.deletePhone = deletePhone;
 
         init();
 
         /////////////
 
         function init () {
+            _getPhoneList();
+        }
+
+        function _getPhoneList () {
             phoneAPI.getPhones()
                 .then(function (data) {
                     vm.phones = data;
                 });
+        }
+
+        function deletePhone (id, name) {
+            LxNotificationService.confirm('Are your sure?',
+                'All information about [' + name + '] will be REMOVED!',
+                {cancel:'cancel', ok:'delete'},
+                function (answer) {
+                    if (answer) {
+                        _doDelete(id);
+                    }
+                }
+            );
+        }
+
+        function _doDelete (id) {
+            phoneAPI.removePhone(id)
+                .then(success)
+                .catch(error);
+
+            function success (data) {
+                _getPhoneList();
+            }
+
+            function error (reason) {
+                var message = ajaxErrorHanlder.getMessage(reason);
+                LxNotificationService.alert('Delete phone error', message, 'OK', function () {
+                    _getPhoneList();
+                });
+            }
         }
 
     }
