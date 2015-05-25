75512acf0e55ba0b8dd197cfeadeadd999ff0567
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-05-25 20:43:42 +0800

    finish add phone feature

--- app/templates/client/source/app/phone/phone.add.controller.js
+++ app/templates/client/source/app/phone/phone.add.controller.js
@@ -5,10 +5,14 @@
         .module('app.phone')
         .controller('PhoneAddController', PhoneAddController);
 
-    PhoneAddController.$inject = ['phoneAPI'];
+    PhoneAddController.$inject = ['phoneAPI', '$state', 'ajaxErrorHanlder',
+        'LxNotificationService'];
     /* @ngInject */
-    function PhoneAddController (phoneAPI) {
+    function PhoneAddController (phoneAPI, $state, ajaxErrorHanlder,
+        LxNotificationService) {
         var vm = this;
+
+        vm.addNewPhone = addNewPhone;
 
         init();
 
@@ -16,6 +20,22 @@
 
         function init () {
             vm.phone = {};
+            vm.state = 'add';
+        }
+
+        function addNewPhone (phone) {
+            phoneAPI.addNewPhone(phone)
+                .then(success)
+                .catch(error);
+
+            function success (data) {
+                $state.go('root.phone');
+            }
+
+            function error (reason) {
+                var message = ajaxErrorHanlder.getMessage(reason);
+                LxNotificationService.alert('Add phone error', message, 'OK');
+            }
         }
 
     }
