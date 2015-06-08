40fc70355708802bec422b443abae820761e7820
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-06-08 23:00:46 +0800

    add common e2e function to helper.js

---
+++ app/templates/client/source/test/e2e/helper.js
@@ -0,0 +1,46 @@
+/* global $ */
+'use strict';
+
+module.exports = function () {
+    return {
+        gotoUrl: gotoUrl,
+        getHeader: getHeader,
+        takeScreenshotIfFail: takeScreenshotIfFail
+    };
+
+    //////////
+
+    function gotoUrl (url) {
+        browser.get(browser.baseUrl + '/' + url);
+    }
+
+    function getHeader () {
+        return {
+            'title': $('.header-title > a'),
+            'loginBtn': $('.header-login'),
+            'userName': $('.header-user-name'),
+            'dropdown': $('.header-dropdown'),
+            'dropdownToggle': $('.dropdown-toggle'),
+            'dropdownMenu': $('.header-dropdown-menu'),
+            'logoutLink': $('.logout-link')
+        };
+    }
+
+    function takeScreenshotIfFail () {
+        var fs = require('fs');
+        var currentSpec = jasmine.getEnv().currentSpec;
+        var passed = currentSpec.results().passed();
+        if (!passed) {
+            browser.takeScreenshot().then(function (png) {
+                browser.getCapabilities().then(function (capabilities) {
+                    var browserName = capabilities.caps_.browserName;
+                    var filename = browserName + '-' +
+                        currentSpec.description.replace(/[ :]/, '-') + '.png';
+                    var stream = fs.createWriteStream(browser.params.screenShotDir + filename);
+                    stream.write(new Buffer(png, 'base64'));
+                    stream.end();
+                });
+            });
+        }
+    }
+};
