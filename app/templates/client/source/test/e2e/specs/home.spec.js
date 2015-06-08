ab77f053728ded2b7a07dbb57e59c1212f75ee89
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-06-08 23:01:08 +0800

    add e2e test for home page

---
+++ app/templates/client/source/test/e2e/specs/home.spec.js
@@ -0,0 +1,60 @@
+/* global $ */
+'use strict';
+
+// page object
+var HomePage = function () {
+    var self = this;
+
+    self.url = '';
+    self.ele = _getAllElements();
+    self.load = load;
+
+    ////////
+
+    function _getAllElements () {
+        var header = browser._.getHeader();
+        return {
+            'headerTitle': header.title,
+            'headerLoginBtn': header.loginBtn,
+            'mainTitle': $('.card > .title'),
+            'subTitle': $('.card > .subtitle'),
+            'getStartedBtn': $('.card > a')
+        };
+    }
+
+    function load () {
+        browser._.gotoUrl(self.url);
+    }
+};
+
+module.exports = HomePage;
+
+// test scenarios
+describe('Home Page:', function () {
+    var page;
+    beforeEach(function () {
+        page = new HomePage();
+        page.load();
+    });
+
+    afterEach(function () {
+        browser._.takeScreenshotIfFail();
+    });
+
+    it('should display correct header', function () {
+        expect(page.ele.headerTitle.getText()).toEqual('Aio Angular App');
+        expect(page.ele.headerLoginBtn.isDisplayed()).toBe(true);
+    });
+
+    it('should display correct tilte and sub title', function () {
+        expect(page.ele.mainTitle.getText()).toEqual('Aio Angular App');
+        expect(page.ele.subTitle.getText()).toEqual(
+            'Awesome web app built on AngularJS & Material Design.');
+    });
+
+    it('should go to login page if click get started', function () {
+        page.ele.getStartedBtn.click();
+        expect(browser.getCurrentUrl()).toMatch(/login/);
+    });
+
+});
