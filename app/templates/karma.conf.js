04306b746d76a83d5c0c08fdd97d120cd62a4350
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-10-08 23:40:06 +0800

    Karma can work
    1. upgrade some npm package
    2. use karma-webpack to config karma
    3. put unit test files in the same folder as the original file, not in test/unit/specs folder any more
    4. update gitignore file to reflect folder change
    5. NOTE: use preprocessor={}, and preprocessor[varibale]=xxx when using variable as key of preprocessor object. I waste many hours on it, and the error message is not useful.

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -1,17 +1,76 @@
+/* eslint-disable */
+var path = require('path');
+var args = require('yargs').argv;
+
+// var specFileFilter = 'source/test/unit-test.index.js';
+var specFileFilter = 'source/app/**/*.spec.js';
+var singleRun = !args.watch;
+var browser = args.watch ? 'PhantomJS' : 'Chrome';
+
+var include = [
+    path.resolve('./source/app')
+];
+// only use coverage/junit in non-watch mode
+var preLoaders = !args.watch ? [
+    // Process test code with Babel
+    {test: /\.spec\.js$/, loader: 'babel', include: include},
+    // Process all non-test code with Isparta
+    {test: /\.js$/, loader: 'isparta', include: include, exclude: /\.spec\.js$/}
+] : [
+    {test: /\.js$/, loader: 'babel', include: include}
+];
+var reporters = args.watch ? [
+    'mocha'
+] : [
+    'mocha', 'coverage', 'junit'
+];
+
+var loaders = [
+    {test: /\.styl$/, loader: 'style!css!stylus'},
+    {test: /\.jade$/, loader: 'jade'},
+    {test: /\.(png|jpg)$/, loader: 'null'}
+];
+var processors = {};
+processors[specFileFilter] = ['webpack'];
+
 module.exports = function (config) {
-
-    var gulpConfig = require('./gulp.config')();
-
     config.set({
-        basePath: gulpConfig.root,
+        basePath: '.',
         frameworks: ['jasmine'],
-        exclude: gulpConfig.karma.exclude,
-        files: gulpConfig.karma.files,
-        preprocessors: gulpConfig.karma.preprocessors,
-        reporters: ['progress', 'coverage', 'junit', 'mocha'],
-        coverageReporter: gulpConfig.karma.coverage,
-        junitReporter: gulpConfig.karma.junit,
+        exclude: [],
+        files: [
+            specFileFilter
+        ],
+        webpack: {
+            devtool: 'eval',
+            module: {
+                preLoaders: preLoaders,
+                loaders: loaders
+            },
+            cache: true
+        },
+        webpackMiddleware: {
+            stats: {
+                chunkModules: false,
+                colors: true
+            }
+        },
+        preprocessors: processors,
+        reporters: reporters,
+        coverageReporter: {
+            dir: 'source/test/unit/results/coverage',
+            reporters: [
+                {type: 'html', subdir: '.'},
+                {type: 'text-summary'}
+            ]
+        },
+        junitReporter: {
+            outputDir: 'source/test/unit/results/junit'
+        },
         reportSlowerThan: 500,
-        browsers: ['chrome']
+        singleRun: singleRun,
+        browsers: [
+            browser
+        ]
     });
 };
