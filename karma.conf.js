39febe0a4e0a8b865c1607a4151b5eb09da463a1
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-10-14 23:34:49 +0800

    change unit test logic
    1. show text coverage in watch mode
    2. show text/html coverage, junit report in single run mode

--- karma.conf.js
+++ karma.conf.js
@@ -12,21 +12,13 @@ var browser = args.watch ? 'PhantomJS' : 'Chrome';
 var include = [
     path.resolve('./source')
 ];
-// only use coverage/junit in non-watch mode
-var preLoaders = !args.watch ? [
+
+var preLoaders = [
     // Process test code with Babel
     {test: /\.spec\.js$/, loader: 'babel', include: include},
     // Process all non-test code with Isparta
     {test: /\.js$/, loader: 'isparta', include: include, exclude: /\.spec\.js$/}
-] : [
-    {test: /\.js$/, loader: 'babel', include: include}
 ];
-var reporters = args.watch ? [
-    'mocha'
-] : [
-    'mocha', 'coverage', 'junit'
-];
-
 var loaders = [
     {test: /\.styl$/, loader: 'style!css!stylus'},
     {test: /\.jade$/, loader: 'jade'},
@@ -35,6 +27,19 @@ var loaders = [
 var processors = {};
 processors[unitTestEntry] = ['webpack', 'sourcemap'];
 processors['source/app/**/*.js'] = ['webpack', 'sourcemap'];
+
+// for watch mode, only show text coverage
+var reporters = args.watch ? [
+    'mocha', 'coverage'
+] : [
+    'mocha', 'coverage', 'junit'
+];
+var coverageReporters = args.watch ? [
+    {type: 'text-summary'}
+] : [
+    {type: 'html', subdir: '.'},
+    {type: 'text-summary'}
+];
 
 module.exports = function (config) {
     config.set({
@@ -69,10 +74,7 @@ module.exports = function (config) {
         reporters: reporters,
         coverageReporter: {
             dir: 'source/test/unit/results/coverage',
-            reporters: [
-                {type: 'html', subdir: '.'},
-                {type: 'text-summary'}
-            ]
+            reporters: coverageReporters
         },
         junitReporter: {
             outputDir: 'source/test/unit/results/junit'
