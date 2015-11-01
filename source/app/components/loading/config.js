94ee5af0c71049f999eb4dd2e1105f2e37e5e12e
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-11-01 21:39:23 +0800

    add max-len 120 limit for eslint

--- source/app/components/loading/config.js
+++ source/app/components/loading/config.js
@@ -1,6 +1,10 @@
 function appLoadingConfig (cfpLoadingBarProvider) {
     cfpLoadingBarProvider.includeSpinner = false;
-    cfpLoadingBarProvider.loadingBarTemplate = '<div class="loading-view"><div class="progress"><div class="indeterminate"></div></div></div>';
+    cfpLoadingBarProvider.loadingBarTemplate = [
+        '<div class="loading-view">',
+        '<div class="progress"><div class="indeterminate"></div></div>',
+        '</div>'
+    ].join('');
 }
 
 appLoadingConfig.$inject = ['cfpLoadingBarProvider'];
