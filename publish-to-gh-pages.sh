1d22ca62aa1d8632565cfbfb7532d1c5ed16d63a
PinkyJie <pilixiaoxuanfeng@gmail.com>
2015-11-26 23:06:13 +0800

    travis ci support (#4)
    1. will run unit test first
    2. deploy unit test coverage report/build files to gh-pages branch

--- publish-to-gh-pages.sh
+++ publish-to-gh-pages.sh
@@ -1,14 +1,34 @@
-git stash
-git apply html5-mode-patch.diff
-cd app/templates/
-gulp build:prod --mock
-cd ../../
-git apply -R html5-mode-patch.diff
-rm -rf static/ index.html
-cp -r app/templates/client/build/prod/* .
-git add static/ index.html
-git commit -m "update site `date +"%Y-%m-%d %H:%M"`"
-git push origin gh-pages
-git push gitcafe gh-pages:gitcafe-pages
-git checkout master
-git stash pop
+#!/bin/bash
+set -ev
+
+# run unit test
+npm test
+
+# run build
+npm build
+
+# copy unit test coverage report to build folder
+cp -r ./source/test/unit/results/coverage ./build
+# push build folder to github
+cd buid
+
+# git init
+git init
+# inside this git repo we'll pretend to be a new user
+git config user.name "Travis CI"
+git config user.email "travis@pinkyjie.com"
+
+# apply # style router for SPA
+git apply -R ./html5-router-patch.diff
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy at `date +"%Y-%m-%d %H:%M"`"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GitHub_TOKEN}@${GitHub_REF}" master:gh-pages > /dev/null 2>&1
+
