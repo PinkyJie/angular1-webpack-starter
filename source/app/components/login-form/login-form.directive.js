4d69aaea239e465c094de33e41e667e4b9d1b401
PinkyJie <pilixiaoxuanfeng@gmail.com>
2016-02-12 16:07:37 +0800

    do not abuse = binding

--- source/app/components/login-form/login-form.directive.js
+++ source/app/components/login-form/login-form.directive.js
@@ -10,7 +10,7 @@ function LoginFormDirective () {
         bindToController: {
             needCheckLogin: '=',
             userInfo: '=',
-            routeAfterLogin: '=',
+            routeAfterLogin: '@',
             loginError: '='
         },
         template: loginFormHtml
